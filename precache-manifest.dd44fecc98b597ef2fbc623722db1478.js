b529c50470e309bee397bf57a341626bf703aecf
Donald Gjeloshi <dongje11@htl-shkoder.com>
2019-12-10 17:52:19 +0100

    Publish (add new Keyboard View)

--- precache-manifest.dd44fecc98b597ef2fbc623722db1478.js
+++ precache-manifest.dd44fecc98b597ef2fbc623722db1478.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ab1e16524642aba87c24",
-    "url": "/eeger-public/css/app.a656e0f3.css"
+    "revision": "f32b182b28fa8ca507ec",
+    "url": "/eeger-public/css/app.75ed323d.css"
   },
   {
-    "revision": "a7d0809a7cd98b5b64dbc54ce7df40e5",
+    "revision": "41a22b5da94cb1669bbd096225f00276",
     "url": "/eeger-public/index.html"
   },
   {
-    "revision": "ab1e16524642aba87c24",
-    "url": "/eeger-public/js/app.e99202cf.js"
+    "revision": "f32b182b28fa8ca507ec",
+    "url": "/eeger-public/js/app.1880cac0.js"
   },
   {
-    "revision": "7fd75017507d98394fac",
-    "url": "/eeger-public/js/chunk-vendors.bec3dd0d.js"
+    "revision": "3ad4c91751cf84cfdc94",
+    "url": "/eeger-public/js/chunk-vendors.4189d4cf.js"
   },
   {
     "revision": "764d92c3c1e4b0de2f764dae48e87f3a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/eeger-public/precache-manifest.a607569346c1059ca5a8edf99cd694a1.js"
+  "/eeger-public/precache-manifest.dd44fecc98b597ef2fbc623722db1478.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "eeger"});
